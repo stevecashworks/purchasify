745687441a7fa11c14bfa48ca19400a4aff7b3b0
stevecashworks <chigbustephennamdi@gmail.com>
2023-03-25 00:57:43 +0100

    Api links were Updated

--- front/src/pages/user/login.jsx
+++ front/src/pages/user/login.jsx
@@ -1,7 +1,7 @@
 import {useRef}from 'react';
 import {Link} from 'react-router-dom'
 import styled from 'styled-components'
-export const apiEntry=`http://localhost:8080/api/v3`
+import { ApiEntry } from '../../App';
 const Container=styled.div`
 display:grid;
 place-items:center;
