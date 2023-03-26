d83d5f87c9a8a64215e04fc466ddaab1c8a7be08
stevecashworks <chigbustephennamdi@gmail.com>
2023-03-26 02:40:31 +0100

    corrections were made

--- front/src/pages/admin/fetchProducts.js
+++ front/src/pages/admin/fetchProducts.js
@@ -1,5 +1,6 @@
- const fetchProducts=async()=>{
-    const allProducts= await fetch("http://localhost:8080/api/v3/products/all")
+import {ApiEntry} from '../../App'
+const fetchProducts=async()=>{
+    const allProducts= await fetch(`${ApiEntry}/api/v3/products/all`)
     .then(res=>res.json())
     .then(data=>data)
     return allProducts
